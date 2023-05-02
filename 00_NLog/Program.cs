f6dd67d73a82edd86bb299e01f17098b9e8e7e76
kinara <kinara.varga@outlook.com>
2023-05-02 11:41:35 +0700

    2nd commit

--- 00_NLog/Program.cs
+++ 00_NLog/Program.cs
@@ -3,21 +3,23 @@ using NLog;
 
 namespace NLogExample
 {
-    class Program
-    {
-        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+	class Program
+	{
+		private static readonly Logger logger = LogManager.GetCurrentClassLogger();
 
-        static void Main(string[] args)
-        {
-            logger.Info("Program started.");
+		static void Main(string[] args)
+		{
+			logger.Info("Program started.");
+			logger.Fatal("FATAL MESSAGE");
+			logger.Error("ERROR MESSAGE");
+			logger.Warn("WARN MESSAGE");
+			int x = 5;
+			int y = 10;
+			int result = x * y;
 
-            int x = 5;
-            int y = 10;
-            int result = x * y;
+			logger.Info($"The result of the calculation is: {result}");
 
-            logger.Info($"The result of the calculation is: {result}");
-
-            logger.Info("Program finished.");
-        }
-    }
+			logger.Info("Program finished.");
+		}
+	}
 }
